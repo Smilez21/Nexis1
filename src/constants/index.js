1c6e577ebfae63d4bcdb6ab269da7991f27d78ef
smilez21 <virtuousajor394@gmail.com>
2022-11-14 09:30:39 +0000

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Gallery",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Printing, Branding & Logos",
+    content:
+      "It’s through mistakes that you actually can grow. You have to get bad in order to get good",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Flyers & Backdrops",
+    content:
+      "There are three responses to a piece of design: yes, no, and WOW! Wow is the one to aim for.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Graphic Designs & Photoshop",
+    content:
+      "The role of the designer is that of a good, thoughtful host anticipating the needs of his guests.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Nexis is not only a pleasure to work with but he has the unique ability to take an idea and expand it into something that exceeds your expectations.",
+    name: "Smilez Virtuous",
+    title: "Customer",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "By far the easiest graphic designer I’ve had the pleasure of working with. He was very insightful from the beginning even when I wasn’t sure what I wanted and, worked through tough concepts with me and delivered exactly the logo I wanted in the end.",
+    name: "Dawal Wisdom",
+    title: "Customer",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "We had a fantastic experience working with Nexis throughout the whole graphic design process. He communicates clearly and with expedience. Nexis shows his talent by being able to take our vision/mission and building it into a logo that captures it precisely.",
+    name: "Ponfa Miri",
+    title: "Founder PPM Fundation",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "1000+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Designs",
+    value: "1000+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
